dd52cbcc9f20c57d066e1244bb5cda7fed3a6e41
patiuma666 <patiuma666@gmail.com>
2017-07-06 17:07:38 +0530

    Add files via upload

---
+++ InitializeArray.java
@@ -0,0 +1,22 @@
+package InitialisingArray;
+/* we are taking an array 'a' of size 10 to initialize the length in array
+
+*/
+public class InitializeArray {
+
+	public static void main(String []args){
+		
+		int a[]=new int[10];
+		System.out.println("even numbers from 1 to 10 are: ");
+		for ( int i = 1; i<a.length; i++){
+						
+			if (i % 2==0){
+				System.out.println( i);
+			}
+            }
+ 
+        }
+		
+	
+
+}
